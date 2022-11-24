caf6325e969e8904ef58171c1230d30ee29be5a4
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2022-11-24 10:07:52 +0530

    more queries added

--- basic-table.sql
+++ basic-table.sql
@@ -3,3 +3,8 @@ CREATE TABLE studenst (
   name VARCHAR(100),
   city VARCHAR(50)
 );
+
+CREATE TABLE cities (
+  id INT
+  name VARCHAR(50)
+)
---
+++ insert-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (id, name, city) VALUES
+(1, "Zakeer", "Guntur"),
+(2, "Syed", "Hyderabad"),
+(3, "Hussain", "Ongole"),
